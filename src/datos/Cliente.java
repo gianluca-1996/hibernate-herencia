f808b140cc79222d30ba76944090426311e6aed1
Gian <Gian.cambareri@outlookcom>
2021-04-23 18:56:33 -0300

    commit inicial

---
+++ src/datos/Cliente.java
@@ -0,0 +1,59 @@
+package datos;
+
+public class Cliente {
+
+	private int idCliente;
+	private String nroCliente;
+	
+	public Cliente() {}
+	
+	public Cliente(String nroCliente)
+	{
+		super();
+		this.nroCliente = nroCliente;
+	}
+	
+	public int getIdCliente()
+	{
+		return this.idCliente;
+	}
+	
+	protected void setidCliente(int idCliente)
+	{
+		this.idCliente = idCliente;
+	}
+	
+	public String getNroCliente() 
+	{
+		return this.nroCliente;
+	}
+	
+	public void setNroCliente(String nroCliente)
+	{
+		this.nroCliente = nroCliente;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", nroCliente=" + nroCliente + "]";
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
